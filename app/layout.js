81954de2a922f01d4779b4b8c6153e35130682ba
Batuhan Y. Yılmaz <batuhan@thebatuhansnetwork.xyz>
2023-11-26 17:20:23 +0300

    Move Files

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,7 @@
+import Header from "@/components/body/header";
+import Footer from "@/components/body/footer";
+
+import { SessionProvider } from "next-auth/react"
 import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -11,7 +15,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`bg-[#0E0C07] text-[#D1D0CB]`}>
+        <div className="h-screen">
+            <Header />
+            <div className={`h-full`}>
+                {children}
+            </div>
+            <Footer />
+        </div>
+      </body>
     </html>
   )
 }
