1517413074a18a08aa2f8a28ca81016d62d6d751
Batuhan Y. Yilmaz <batuhan@thebatuhansnetwork.xyz>
2023-12-14 16:58:14 +0300

    Did something that I don't even know

--- components/blog/blog-card.jsx
+++ components/blog/blog-card.jsx
@@ -10,19 +10,24 @@ import { Button } from "../ui/button"
 
 function BlogCard({ title, description, link, date, image }) {
     return (
-        <Card className="rounded-2xl bg-stone-700 border-none h-[300px]">
-            <CardHeader className="flex ">
-                <AspectRatio ratio={16 / 9}>
-                    <Image src="/cards/map.png" alt="map image" width={1280} height={720} className="rounded-2xl object-cover" />
-                </AspectRatio>
-                <CardTitle>{title}</CardTitle>
-                <CardDescription>{description}</CardDescription>
-            </CardHeader>
-            <CardFooter>
-                <Button>
-                    Test Button
-                </Button>
-            </CardFooter>
+        <Card className="rounded-2xl bg-stone-700 border-none h-auto">
+            <CardContent>
+                <div className="flex flex-col md:flex-row max-h-full pt-5">
+                    <div className="max-w-[300px] mx-auto md:mx-0 md:mr-5">
+                        <Image
+                            className="rounded-2xl md:h-auto max-w-3xl min-w-[300px] mx-auto"
+                            src={image}
+                            width={300}
+                            height={300}
+                            alt="" 
+                        />
+                    </div>
+                    <div className="mt-5 md:mt-0 mx-auto md:mx-0">
+                        <CardTitle>{title}</CardTitle>
+                        <CardDescription>{description}</CardDescription>
+                    </div>
+                </div>
+            </CardContent>
         </Card>
     )
 }
