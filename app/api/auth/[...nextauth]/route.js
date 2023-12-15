0b64661cc883ac68d068fdba3a9de12e9f140f49
Batuhan Y. Yılmaz <batuhan@thebatuhansnetwork.xyz>
2023-12-15 19:16:14 +0300

    [Backend] Added NextAuth route handler

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import DiscordProvider from "next-auth/providers/discord"
+import { db } from "@/lib/db"
+
+const prisma = db()
+
+export const authOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        DiscordProvider({
+            clientId: process.env.DISCORD_CLIENT_ID,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET,
+            profile(profile) {
+                return {
+                    id: profile.id,
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.picture,
+                    role: profile.role ?? 'USER'
+                }
+            }
+        }),
+    ],
+    callbacks: {
+        session({ session, user }) {
+            session.user.role = user.role
+            return session
+        }
+    },
+    pages: {
+        signIn: '/auth/signin'
+    },
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
